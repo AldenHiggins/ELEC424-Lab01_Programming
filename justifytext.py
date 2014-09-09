142c6b173f30768f751a0cde964171ff72e4a03a
Alden Higgins <jah17@rice.edu>
2014-09-08 23:51:58 -0500

    Real commit

--- justifytext.py
+++ justifytext.py
@@ -1,10 +1,57 @@
+#! /usr/bin/python
 import sys
+import pdb
 
-# 
-# INSERT DESCRIPTION HERE
-#
+# Change the text in file_name such that every line is of length
+# justify_length
 def justify(file_name, justify_length):
-    # COMPLETE ME
+	file = open(file_name,'r+')
+	newFileText = ""
+	for line in file:
+		newFileText += justifyHelper(line,file,justify_length)
+	file.seek(0,0)
+	file.write(newFileText)
+	file.close()
+
+def justifyHelper(line, file, justify_length):
+	# If the line is greater than the justify_length, split off the line
+	# at the justify length and justify the rest of the line on another line.
+	if (len(line) > justify_length):
+		return justifyHelper2(line[0:justify_length - 2] + "\n", file, justify_length) + justifyHelper(line[justify_length - 2:len(line)], file, justify_length)
+	else:
+		return justifyHelper2(line, file, justify_length)
+
+def justifyHelper2(line, file, justify_length):
+	numWords = len(line.split())
+	spacesPerWord = (justify_length - len(line))/(numWords)
+	newLine = ""
+	wordFound = False
+	wordsFound = 0
+	spacesAdded = 0
+	for letter in line:
+		newLine += letter
+		if (letter == "\n"):
+			break
+		if (wordFound == False):
+			if (letter != ' '):
+				wordFound = True
+				wordsFound += 1
+		if (wordFound):
+			if (letter == ' '):
+				if (wordsFound == numWords - 1):
+					for i in range(justify_length - len(line) - spacesAdded):
+						newLine += ' '
+				else:
+					for i in range(spacesPerWord):
+						newLine += ' '
+						spacesAdded += 1
+				wordFound = False
+	#pdb.set_trace()
+	#print "old: " + line
+	#print len(newLine)
+	#print "new: " + newLine
+	#file.write(newLine)
+	return newLine
 
 # Program justifies text contents of a given file
 # This is the actual code that gets run when the
@@ -30,3 +77,4 @@ if __name__ == "__main__":
         exit('Input error. Example input: justifytext -f mytextfile -l 80')
         
     justify(file_name, length)
+	
---
+++ testJustify.py
@@ -0,0 +1,4 @@
+import pdb
+import justifytext
+
+pdb.run('justifytext.justify("justifyThis.txt",40)')
